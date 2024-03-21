62ac0b1ab8109aae4ccb913091f5b1066958525e
Patrick Uiterwijk <patrick@puiterwijk.org>
2024-03-21 14:03:17 +0000

    Use automatic cert injection
    Signed-off-by: Patrick Uiterwijk <patrick@puiterwijk.org>

--- install.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/bash -xe
-SERVICE_CA_KEY=$(oc -n openshift-service-ca get secret/signing-key -o go-template='{{index .data "tls.crt"}}')
-oc apply -f manifests/deploy.yaml
-cat manifests/webhook.yaml | sed -e 's/SERVICECACRT/$SERVICE_CA_KEY/' | oc apply -f -
